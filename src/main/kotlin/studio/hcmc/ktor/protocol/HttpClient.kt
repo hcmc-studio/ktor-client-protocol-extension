7a612e8299698b5378500001ce54dc39ce468f6f
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-10 11:11:41 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/ktor/protocol/HttpClient.kt
+++ src/main/kotlin/studio/hcmc/ktor/protocol/HttpClient.kt
@@ -4,10 +4,13 @@ import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.util.*
 import studio.hcmc.kotlin.protocol.DataTransferObject
 
-suspend inline fun HttpClient.get(urlString: String, vararg parameters: Pair<String, Any?>, block: HttpRequestBuilder.() -> Unit = {}): HttpResponse {
+suspend inline fun HttpClient.get(
+    urlString: String,
+    vararg parameters: Pair<String, Any?>,
+    block: HttpRequestBuilder.() -> Unit = {}
+): HttpResponse {
     return get {
         url(urlString)
         for ((name, value) in parameters) {
@@ -26,7 +29,11 @@ suspend inline fun HttpClient.get(urlString: String, vararg parameters: Pair<Str
     }
 }
 
-suspend inline fun <reified T : DataTransferObject> HttpClient.post(urlString: String, dto: T, block: HttpRequestBuilder.() -> Unit = {}): HttpResponse {
+suspend inline fun <reified T : DataTransferObject> HttpClient.post(
+    urlString: String,
+    dto: T,
+    block: HttpRequestBuilder.() -> Unit = {}
+): HttpResponse {
     return post {
         url(urlString)
         setBody(dto)
@@ -36,7 +43,11 @@ suspend inline fun <reified T : DataTransferObject> HttpClient.post(urlString: S
     }
 }
 
-suspend inline fun <reified T : DataTransferObject> HttpClient.put(urlString: String, dto: T, block: HttpRequestBuilder.() -> Unit = {}): HttpResponse {
+suspend inline fun <reified T : DataTransferObject> HttpClient.put(
+    urlString: String,
+    dto: T,
+    block: HttpRequestBuilder.() -> Unit = {}
+): HttpResponse {
     return put {
         url(urlString)
         setBody(dto)
@@ -46,7 +57,11 @@ suspend inline fun <reified T : DataTransferObject> HttpClient.put(urlString: St
     }
 }
 
-suspend inline fun <reified T : DataTransferObject> HttpClient.patch(urlString: String, dto: T, block: HttpRequestBuilder.() -> Unit = {}): HttpResponse {
+suspend inline fun <reified T : DataTransferObject> HttpClient.patch(
+    urlString: String,
+    dto: T,
+    block: HttpRequestBuilder.() -> Unit = {}
+): HttpResponse {
     return patch {
         url(urlString)
         setBody(dto)
---
+++ src/main/kotlin/studio/hcmc/ktor/protocol/HttpResponse.kt
@@ -0,0 +1,36 @@
+package studio.hcmc.ktor.protocol
+
+import io.ktor.client.call.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.utils.io.errors.*
+import studio.hcmc.kotlin.protocol.ValueObject
+import studio.hcmc.kotlin.protocol.io.Response
+
+object HttpResponseUtil {
+    var fallbackHandler: ((errorResponse: Response.Error) -> Throwable?)? = null
+}
+
+suspend inline fun HttpResponse.bodyAsEmpty(): Result<Response.Empty> {
+    return bodyAsResponse()
+}
+
+suspend inline fun <reified VO : ValueObject> HttpResponse.bodyAsObject(): Result<Response.Object<VO>> {
+    return bodyAsResponse()
+}
+
+suspend inline fun <reified VO : ValueObject> HttpResponse.bodyAsArray(): Result<Response.Array<VO>> {
+    return bodyAsResponse()
+}
+
+suspend inline fun <reified R : Response<*>> HttpResponse.bodyAsResponse(): Result<R> {
+    if (status.isSuccess()) {
+        return Result.success(body())
+    } else {
+        val errorResponse = try { body<Response.Error>() } catch (e: Throwable) { return Result.failure(e) }
+        val fallbackHandler = HttpResponseUtil.fallbackHandler ?: return Result.failure(IOException("Uncaught error: $errorResponse"))
+        val throwable = fallbackHandler(errorResponse) ?: return Result.failure(IOException("Unhandled error: $errorResponse"))
+
+        return Result.failure(throwable)
+    }
+}
