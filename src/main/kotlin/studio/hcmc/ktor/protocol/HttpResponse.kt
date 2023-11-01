260d1d52e8267898991de65fbfaee0d4d3d8ffe4
Ji-Hwan KIM <nodealdev@gmail.com>
2023-11-01 16:09:37 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/ktor/protocol/HttpResponse.kt
+++ src/main/kotlin/studio/hcmc/ktor/protocol/HttpResponse.kt
@@ -19,36 +19,13 @@ suspend inline fun <reified R> HttpResponse.bodyAsArray(): Result<Response.Array
 }
 
 suspend inline fun <reified R : Response<*>> HttpResponse.bodyAsResponse(): Result<R> {
-    if (status.isSuccess()) {
-        return Result.success(body())
+    return if (status.isSuccess()) {
+        Result.success(body())
     } else {
-        val errorResponse = runCatching { body<Response.Error>() }.getOrElse {
-            return Result.failure(IOException("Response is not instance of Response.Error", it))
+        try {
+            Result.failure(body<Response.Error>().result)
+        } catch (e: Throwable) {
+            Result.failure(IOException("Response is not instance of Response.Error", e))
         }
-        val errorClass = runCatching { Class.forName(errorResponse.className) }.getOrElse {
-            val tokens = errorResponse.className.split(".")
-            val nestedClassName = tokens.subList(0, tokens.size - 1).joinToString(".") + "$" + tokens.last()
-            runCatching { Class.forName(nestedClassName) }.getOrElse {
-                return Result.failure(IOException("Exception class is not found for name: [${errorResponse.className}, $nestedClassName]"))
-            }
-        }
-        val objectInstance = errorClass.kotlin.objectInstance
-        if (objectInstance != null) {
-            val throwable = objectInstance as? Throwable
-                ?: return Result.failure(IOException("Companion INSTANCE is not throwable: class=$errorClass, response=$errorResponse"))
-
-            return Result.failure(throwable)
-        }
-
-        val constructor = errorClass.kotlin.constructors.find { it.parameters.isEmpty() } ?:
-            return Result.failure(IOException("Cannot find empty constructor: class=$errorClass, response=$errorResponse"))
-        val instance = runCatching { constructor.call() }.getOrElse {
-            return Result.failure(IOException("Cannot call constructor: class=$errorClass, response=$errorResponse", it))
-        }
-        val throwable = instance as? Throwable
-            ?: return Result.failure(IOException("Created instance is not throwable: class=$errorClass, response=$errorResponse"))
-
-        return Result.failure(throwable)
-
     }
 }
