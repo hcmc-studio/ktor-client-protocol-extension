f44352d33944c8df7fcc14497e5a6ed8db338c67
Ji-Hwan KIM <nodealdev@gmail.com>
2024-02-16 15:21:49 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/ktor/protocol/HttpClient.kt
+++ src/main/kotlin/studio/hcmc/ktor/protocol/HttpClient.kt
@@ -6,35 +6,35 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import studio.hcmc.kotlin.protocol.io.Response
 
-suspend inline fun <reified T> HttpClient.getObject(
+suspend fun <T> HttpClient.getObject(
     urlString: String,
     block: HttpRequestBuilder.() -> Unit = {}
 ): T {
     return get(urlString, block).interceptError<Response.Object<T>>().result
 }
 
-suspend inline fun <reified T> HttpClient.getArray(
+suspend fun <T> HttpClient.getArray(
     urlString: String,
     block: HttpRequestBuilder.() -> Unit = {}
 ): List<T> {
     return get(urlString, block).interceptError<Response.Array<T>>().result
 }
 
-suspend inline fun <reified T> HttpClient.postObject(
+suspend fun <T> HttpClient.postObject(
     urlString: String,
     block: HttpRequestBuilder.() -> Unit = {}
 ): T {
     return post(urlString, block).interceptError<Response.Object<T>>().result
 }
 
-suspend inline fun <reified T> HttpClient.putObject(
+suspend fun <T> HttpClient.putObject(
     urlString: String,
     block: HttpRequestBuilder.() -> Unit = {}
 ): T {
     return put(urlString, block).interceptError<Response.Object<T>>().result
 }
 
-suspend inline fun <reified T> HttpClient.patchObject(
+suspend fun <T> HttpClient.patchObject(
     urlString: String,
     block: HttpRequestBuilder.() -> Unit = {}
 ): T {
